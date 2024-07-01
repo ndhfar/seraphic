c0a4b401e3faa8ae0892ce0bc426f30abc08136b
ndhfar <nadipadhifa5@gmail.com>
2024-07-01 11:35:11 +0700

    create register and login action

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,29 +1,45 @@
 "use client";
 
 import Link from "next/link";
+import { useActionState } from "react";
+import LoginAction from "./action";
 
 export default function Page() {
+  const [state, formAction, pending] = useActionState(LoginAction, null);
+
   return (
     <main className="space-y-4 ">
       <h1 className="text-2xl font-semibold text-center">Hello Again!</h1>
-      <form action="" className="space-y-2">
+      <form action={formAction} className="space-y-2">
         <InputTemplate
           label="Email"
           name="email"
           type="email"
           icon="bx-envelope"
+          value={state?.data?.email}
         />
         <InputTemplate
           label="Password"
           name="password"
           type="password"
           icon="bx-lock"
+          value={state?.data?.password}
         />
         <div>
-          <button className="mt-2 w-full btn btn-neutral btn-sm">
-            Register
+          <button
+            className="mt-2 w-full btn btn-neutral btn-sm"
+            disabled={pending}
+          >
+            Log In
           </button>
         </div>
+
+        {state?.status === "failed" ? (
+          <StatusTemplate status="failed" message={state.message} />
+        ) : null}
+        {state?.status === "success" ? (
+          <StatusTemplate status="success" message={state.message} />
+        ) : null}
       </form>
       <p className="text-xs text-center">
         Don&apos;t have an account? {""}
@@ -38,7 +54,7 @@ export default function Page() {
   );
 }
 
-const InputTemplate = ({ label, name, type, icon }) => {
+const InputTemplate = ({ label, name, type, icon, value }) => {
   return (
     <div>
       <label className="text-sm">{label}</label>
@@ -49,8 +65,25 @@ const InputTemplate = ({ label, name, type, icon }) => {
           placeholder={name}
           type={type}
           className="outline-none pl-2 text-inherit placeholder: text-sm  placeholder: text-slate-400"
+          defaultValue={value}
         />
       </div>
     </div>
   );
+};
+
+const StatusTemplate = ({ status, message }) => {
+  if (status === "failed") {
+    return (
+      <p className="bg-red-100 text-red-500 text-center py-1 text-sm">
+        {message}
+      </p>
+    );
+  } else if (status === "success") {
+    return (
+      <p className="bg-green-100 text-green-500 text-center py-1 text-sm">
+        {message}
+      </p>
+    );
+  }
 };
